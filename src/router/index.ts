199f5c77296834f516cfe865d85abfebdc79b488
kaoniqiwa <1061886912@qq.com>
2024-04-21 15:01:14 +0800

    layout布局

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,8 +3,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 
-console.log(import.meta.env.BASE_URL)
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
 import * as vue from 'vue'
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
 </script>
 <template>
-  <div>layout</div>
-  <router-view></router-view>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
 <style scoped lang="scss"></style>
